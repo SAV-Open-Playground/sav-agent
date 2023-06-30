506e397704592619f847285dda5a88eb37b54425
MichaelYoungGo <ustb_yhb@163.com>
2023-06-30 16:28:05 +0800

    temporary storage traffic file

---
+++ trafficTools/traffic_sender.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     traffic_sender.py
+   Description :
+   date：          2023/6/20
+-------------------------------------------------
+   Change Activity:
+                   2023/6/20:
+-------------------------------------------------
+"""
+
+
+class TrafficSender:
+    def send(self, src, des, iface, rate):
+        pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ trafficTools/traffiic_receiver.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     traffiic_receiver
+   Description :
+   date：          2023/6/20
+-------------------------------------------------
+   Change Activity:
+                   2023/6/20:
+-------------------------------------------------
+"""
+
+
+class TrafficReceiver:
+    pass
+
+if __name__ == "__main__":
+    pass
