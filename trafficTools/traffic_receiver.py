24ca02cec5f288a232e0fb5a764fc58c91eb5872
MichaelYoungGo <ustb_yhb@163.com>
2023-07-15 15:46:12 +0800

    build new tradffic test by socket, scapy and UDP

--- trafficTools/traffic_receiver.py
+++ trafficTools/traffic_receiver.py
@@ -9,11 +9,27 @@
                    2023/6/20:
 -------------------------------------------------
 """
+import argparse
+from scapy.all import *
+from scapy.layers.inet import IP, ICMP, TCP
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dst')
+parser.add_argument('--src', default=None)
+args = parser.parse_args()
+dst, src = args.dst, args.src
+
+timeout = 10
 
 
+def packet_callback(packet):
+    if packet.haslayer(IP):
+        ip_packet = packet.getlayer(IP)
+        if ip_packet.src == src and ip_packet.dst == dst:
+            print("Get sent packet: ", packet.summary())
 
-class TrafficReceiver:
-    pass
 
-if __name__ == "__main__":
-    pass
+# sniff(filter=f"src host {src} and dst host {dst}", prn=packet_callback, timeout=10)
+
+sniff(filter=f"src host {src} and dst host {dst}", prn=packet_callback)
+print(2)
